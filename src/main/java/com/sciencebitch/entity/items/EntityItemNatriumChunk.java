f74ceaeb554698bf9225022423f051187a764387
Thal1ka <joern.riedl@gmx.de>
2018-07-21 01:06:03 +0200

    ADD: Functionality which lets ignited natrium move

--- src/main/java/com/sciencebitch/entity/items/EntityItemNatriumChunk.java
+++ src/main/java/com/sciencebitch/entity/items/EntityItemNatriumChunk.java
@@ -1,21 +1,59 @@
 package com.sciencebitch.entity.items;
 
+import java.util.Random;
+
 import net.minecraft.entity.item.EntityItem;
 
 public class EntityItemNatriumChunk extends DropItemEntityBase {
 
+	private static final int BURN_TIME = 40;
+
+	private final Random rnd;
+
+	private boolean isIgnited;
+	private int timeSinceIgnition;
+
 	public EntityItemNatriumChunk(EntityItem entityItem) {
 		super(entityItem, entityItem.posX, entityItem.posY, entityItem.posZ);
+
+		rnd = world.rand;
 	}
 
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
 
-		if (isInWater()) {
-
-			this.setDead();
-			this.world.createExplosion(this, this.posX, this.posY + this.height / 16.0F + 1, this.posZ, 1.0F, true);
+		if (isInWater() && !isIgnited) {
+			isIgnited = true;
+			setInfinitePickupDelay();
 		}
+	}
+
+	@Override
+	public void onEntityUpdate() {
+		super.onEntityUpdate();
+
+		if (isIgnited && !world.isRemote) {
+
+			timeSinceIgnition++;
+
+			if (timeSinceIgnition >= BURN_TIME) {
+
+				this.setDead();
+				this.explode();
+
+			} else {
+
+				createBurningMotion();
+			}
+		}
+	}
+
+	private void explode() {
+		this.world.createExplosion(this, this.posX, this.posY + this.height / 16.0F + 1, this.posZ, 1.0F, true);
+	}
+
+	private void createBurningMotion() {
+
 	}
 }
