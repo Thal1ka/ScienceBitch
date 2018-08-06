32a0718be177649fcffdd5d1e1a9836eb0c859c5
Thal1ka <40734551+Thal1ka@users.noreply.github.com>
2018-08-06 07:54:51 +0200

    BUGFIX: Prevent energy loss if storage is full

--- src/main/java/com/sciencebitch/tileentities/generators/TileEntityCombustionGenerator.java
+++ src/main/java/com/sciencebitch/tileentities/generators/TileEntityCombustionGenerator.java
@@ -142,13 +142,13 @@ public class TileEntityCombustionGenerator extends TileEntityGeneratorBase {
 			if (!isWorking()) return;
 		}
 
-		generateEnergy(speedMultiplicator);
 		currentBurnTime += speedMultiplicator;
+		int energyToGenerate = speedMultiplicator;
 
 		if (currentBurnTime >= totalBurnTime) {
 
 			int energyToReset = totalBurnTime - currentBurnTime;
-			generateEnergy(energyToReset);
+			energyToGenerate -= energyToReset;
 
 			currentBurnTime = 0;
 			totalBurnTime = 0;
@@ -156,6 +156,7 @@ public class TileEntityCombustionGenerator extends TileEntityGeneratorBase {
 			handleFuel();
 		}
 
+		generateEnergy(energyToGenerate);
 		ticksBurning++;
 
 		if (ticksBurning % TICKS_MULTIPLIER_INCREASE == 0) {
